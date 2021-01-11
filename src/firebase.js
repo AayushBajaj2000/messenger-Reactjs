f604acd691362aac574fff5bcf3a2f9da239fd42
Aayush Bajaj <59947649+AayushBajaj2000@users.noreply.github.com>
2021-01-11 18:17:25 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,6 @@
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-
-  apiKey: "AIzaSyDcFFqioHXCU-RsUvbhiDoJRDojy1maEQo",
-  authDomain: "facebook-clone-c8aae.firebaseapp.com",
-  databaseURL: "https://facebook-clone-c8aae.firebaseio.com",
-  projectId: "facebook-clone-c8aae",
-  storageBucket: "facebook-clone-c8aae.appspot.com",
-  messagingSenderId: "898891998485",
-  appId: "1:898891998485:web:6e76e6a8efaf8602dbb51c",
-  measurementId: "G-QFYDGJYM8L"
-
-});
+const firebaseApp = firebase.initializeApp({%Your API CONFIG HERE%});
 
 const db = firebaseApp.firestore();
 
